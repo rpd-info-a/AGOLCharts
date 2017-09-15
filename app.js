5a79d306d8954669177d6b2df9c884971d3de6d5
rpd-info-a <joseph.silva@cityofrochester.gov>
2017-09-15 16:20:56 -0400

    changed structure

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get('/', function (req, res) {
 })
 
 app.use(express.static('pub'))
+app.use(express.static('js'))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
